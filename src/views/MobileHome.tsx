5bc8dd7ae7fbc50c70e03350064a0f635c2543cf
chldls153@naver.com <chldls153@naver.com>
2024-06-10 13:20:55 +0900

    mobile home 수정

--- src/views/MobileHome.tsx
+++ src/views/MobileHome.tsx
@@ -1,9 +1,5 @@
 import styled from 'styled-components'
 import home_mobile_image from '../assets/images/home_mobile_1.jpg'
-import Button from '../components/common/Button'
-import { Link } from 'react-router-dom'
-import { RiInstagramFill } from 'react-icons/ri'
-import { FaSquareFacebook, FaYoutube } from 'react-icons/fa6'
 
 export default function MobileHome() {
   return (
@@ -32,105 +28,5 @@ const MoblieSection = styled.section`
 
   &:nth-child(3) {
     height: 100vh;
-  }
-`
-
-const MoblieTextBox = styled.div`
-  display: flex;
-  position: absolute;
-  top: 190px;
-  left: 30px;
-
-  p {
-    line-height: 26px;
-    font-size: 0.7rem;
-    color: rgba(50, 50, 50, 1);
-  }
-
-  &:nth-child(2) {
-    top: 700px;
-  }
-`
-
-const SocialLinks = styled.section`
-  position: absolute;
-  bottom: 180px;
-  left: 222px;
-  font-size: 32px;
-  height: 40px;
-  width: 130px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  a {
-    color: rgba(20, 20, 20, 1);
-  }
-`
-
-const HoverableCon = styled.div``
-
-interface TextConProps {
-  isOnMouse: boolean
-}
-
-const TextCon = styled.div<TextConProps>`
-  overflow: hidden;
-  white-space: nowrap;
-  width: 100%;
-  box-sizing: border-box;
-  position: absolute;
-  top: 180px;
-  z-index: ${props => (props.isOnMouse ? '98' : '100')};
-  background-color: #fff;
-  opacity: ${props => (props.isOnMouse ? '0' : '1')};
-`
-
-const DetailText = styled.div<TextConProps>`
-  position: absolute;
-  top: 200px;
-  z-index: 99;
-  width: 100%;
-  display: flex;
-  align-items: start;
-  justify-content: center;
-  opacity: ${props => (props.isOnMouse ? '1' : '0')};
-  transition: opacity 0.5s ease;
-`
-
-const Detail = styled.div`
-  position: relative;
-  min-width: 500px;
-  height: 210px;
-  display: flex;
-
-  &:first-of-type {
-    border-right: 1px solid rgba(20, 20, 20, 1);
-    margin-right: 60px;
-    padding-right: 60px;
-  }
-
-  span {
-    font-size: 2rem;
-    font-weight: bold;
-  }
-
-  p {
-    line-height: 30px;
-    font-size: 1rem;
-    margin-bottom: 50px;
-    max-width: 400px;
-  }
-
-  div {
-    display: flex;
-    flex-direction: column;
-    margin-left: 70px;
-  }
-
-  button {
-    position: absolute;
-    bottom: 0px;
-    left: 150px;
   }
 `
