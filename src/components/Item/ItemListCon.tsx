7087a2fdff409dc427dea80fefbdc99f7620c210
chldls153@naver.com <chldls153@naver.com>
2024-09-09 23:45:53 +0900

    feat: 메인 페이지에서 아이템을 패치하는 로직을 훅으로 따로 분리해서 사용하고 있었는데 로딩처리하는데 오류가 있어서 이 부분을 수정

--- src/components/Item/ItemListCon.tsx
+++ src/components/Item/ItemListCon.tsx
@@ -1,36 +1,47 @@
-import styled from "styled-components";
-import ItemCard from "./ItemCard";
-import { useQuery } from "react-query";
-import { useAuth } from "../../hooks/useAuth";
-import { fetchCartItems } from "../../config/api/cart/fetchCartItems";
-import { QUERY_KEYS } from "../../config/constants/queryKeys";
-import loadingIcon from "../../assets/images/logo/ball-triangle.svg";
-import { fetchUserLikesInfo } from "../../config/api/user/fetchUserInfo";
-import { useLocation } from "react-router-dom";
-import { useItems } from "../../hooks/useItem";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { sortState } from "../../recoil/atoms/SortState";
-import { useEffect } from "react";
+import { useEffect, useRef } from 'react'
+import styled from 'styled-components'
+import ItemCard from './ItemCard'
+import { useQuery, useInfiniteQuery } from 'react-query'
+import { useAuth } from '../../hooks/useAuth'
+import { fetchCartItems } from '../../config/api/cart/fetchCartItems'
+import { QUERY_KEYS } from '../../config/constants/queryKeys'
+import loadingIcon from '../../assets/images/logo/ball-triangle.svg'
+import { fetchUserLikesInfo } from '../../config/api/user/fetchUserInfo'
+import { useLocation } from 'react-router-dom'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { sortState } from '../../recoil/atoms/SortState'
+
 import {
   sortHighestDiscountRate,
   sortHighestPrice,
   sortItmeByFilterObj,
   sortLowestId,
   sortLowestPrice,
-} from "../../utill/itemSort";
-import { filterState } from "../../recoil/atoms/FilterState";
-import { sortedItemsState } from "../../recoil/atoms/SortedItemsState";
-import { CartItemType } from "../../types/CartItemType";
+} from '../../utill/itemSort'
+import { filterState } from '../../recoil/atoms/FilterState'
+import { sortedItemsState } from '../../recoil/atoms/SortedItemsState'
+import { CartItemType } from '../../types/CartItemType'
+import { fetchItemsPerPage } from '../../config/api/items/fetchItems'
 
 export default function ItemListCon() {
-  const { session } = useAuth();
-  const location = useLocation();
-  const sortValue = useRecoilValue(sortState);
-  const queryParams = new URLSearchParams(location.search);
-  const filterData = useRecoilValue(filterState);
-  const [sortedItems, setSortedItems] = useRecoilState(sortedItemsState);
-  const tabValue = Number(queryParams.get("tab"));
-  const { itemData, loading, hasMore, ref } = useItems(tabValue);
+  const { session } = useAuth()
+  const location = useLocation()
+  const sortValue = useRecoilValue(sortState)
+  const queryParams = new URLSearchParams(location.search)
+  const filterData = useRecoilValue(filterState)
+  const [sortedItems, setSortedItems] = useRecoilState(sortedItemsState)
+  const tabValue = Number(queryParams.get('tab'))
+
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
+    useInfiniteQuery(
+      ['items', tabValue],
+      ({ pageParam = 0 }) => fetchItemsPerPage(pageParam, 10, tabValue),
+      {
+        getNextPageParam: lastPage => lastPage.nextCursor || undefined,
+        staleTime: Infinity,
+        cacheTime: 30 * 60 * 1000,
+      }
+    )
 
   const { data: userLikeData, isLoading: userInfoLoading } = useQuery(
     QUERY_KEYS.USERS,
@@ -40,7 +51,7 @@ export default function ItemListCon() {
       staleTime: Infinity,
       cacheTime: 30 * 60 * 1000,
     }
-  );
+  )
 
   const { data: cartData, isLoading: cartLoading } = useQuery(
     QUERY_KEYS.CART_ITEMS,
@@ -50,33 +61,63 @@ export default function ItemListCon() {
       staleTime: Infinity,
       cacheTime: 30 * 60 * 1000,
     }
-  );
+  )
+
+  const observerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+          fetchNextPage()
+        }
+      },
+      { threshold: 1.0 }
+    )
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current)
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current)
+      }
+    }
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage])
 
   useEffect(() => {
     const fetchFilteredAndSortedItems = async () => {
-      let filteredItems = await sortItmeByFilterObj([...itemData], filterData);
+      if (data) {
+        let filteredItems = await sortItmeByFilterObj(
+          [...data.pages.flatMap(page => page.items)],
+          filterData
+        )
 
-      if (sortValue === "추천순") {
-        filteredItems = sortLowestId(filteredItems);
-      } else if (sortValue === "낮은가격순") {
-        filteredItems = sortLowestPrice(filteredItems);
-      } else if (sortValue === "높은가격순") {
-        filteredItems = sortHighestPrice(filteredItems);
-      } else if (sortValue === "할인률순") {
-        filteredItems = sortHighestDiscountRate(filteredItems);
+        if (sortValue === '추천순') {
+          filteredItems = sortLowestId(filteredItems)
+        } else if (sortValue === '낮은가격순') {
+          filteredItems = sortLowestPrice(filteredItems)
+        } else if (sortValue === '높은가격순') {
+          filteredItems = sortHighestPrice(filteredItems)
+        } else if (sortValue === '할인률순') {
+          filteredItems = sortHighestDiscountRate(filteredItems)
+        }
+
+        setSortedItems(filteredItems)
       }
+    }
 
-      setSortedItems(filteredItems);
-    };
+    if (data) {
+      fetchFilteredAndSortedItems()
+    }
+  }, [sortValue, filterData, data])
 
-    fetchFilteredAndSortedItems();
-  }, [sortValue, filterData, itemData]);
-
-  const cartItems: CartItemType[] = cartData ? cartData.items : [];
+  const cartItems: CartItemType[] = cartData ? cartData.items : []
 
   return (
     <>
-      {cartLoading || (itemData.length === 0 && loading) || userInfoLoading ? (
+      {cartLoading || (isLoading && !data) || userInfoLoading ? (
         <ListCon>
           <ListWrapper>
             <LoadingScreen>
@@ -100,7 +141,7 @@ export default function ItemListCon() {
                   discount,
                   deliveryperiod,
                 }) => {
-                  const isInCart = cartItems.some((item) => item.itemId === id);
+                  const isInCart = cartItems.some(item => item.itemId === id)
                   return (
                     <ItemCard
                       key={id}
@@ -114,22 +155,19 @@ export default function ItemListCon() {
                       userLikeData={userLikeData?.likes}
                       deliveryPeriod={deliveryperiod}
                     />
-                  );
+                  )
                 }
               )
             )}
           </ListWrapper>
-          <LoadingObserver
-            ref={ref}
-            isfirstfetch={String(sortedItems.length === 0)}
-          >
-            {loading && <img src={loadingIcon} alt="loading" />}
-            {!hasMore && <div></div>}
+          <LoadingObserver ref={observerRef}>
+            {isLoading && <img src={loadingIcon} alt="loading" />}
+            {!hasNextPage && <div></div>}
           </LoadingObserver>
         </ListCon>
       )}
     </>
-  );
+  )
 }
 
 const LoadingScreen = styled.div`
@@ -173,7 +211,7 @@ const LoadingScreen = styled.div`
       width: 80px;
     }
   }
-`;
+`
 
 const ListCon = styled.div`
   width: 100%;
@@ -185,27 +223,21 @@ const ListCon = styled.div`
   @media (max-width: 768px) {
     padding-left: 0px;
   }
-`;
+`
 
 const ListWrapper = styled.div`
   display: flex;
   width: 100%;
   flex-wrap: wrap;
   justify-content: flex-start;
-`;
+`
 
-const LoadingObserver = styled.div<{ isfirstfetch: string }>`
+const LoadingObserver = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: ${(props) => (props.isfirstfetch === "true" ? "-100px" : "0px")};
-
-  @media (max-width: 600px) {
-    margin-top: ${(props) =>
-      props.isfirstfetch === "true" ? "-300px" : "0px"};
-  }
-`;
+`
 
 const Empty = styled.div`
   width: 100%;
@@ -218,4 +250,4 @@ const Empty = styled.div`
   @media (max-width: 600px) {
     font-size: 1rem;
   }
-`;
+`
--- src/config/api/items/fetchItems.ts
+++ src/config/api/items/fetchItems.ts
@@ -1,52 +1,52 @@
-import { supabase } from "../../../supabase/supabaseClient";
-import { tabNumberToCategory } from "../../../utill/tabNumberToCategory";
+import { supabase } from '../../../supabase/supabaseClient'
+import { tabNumberToCategory } from '../../../utill/tabNumberToCategory'
 
 export async function fetchItems() {
-  const { data, error } = await supabase.from("items").select("*");
+  const { data, error } = await supabase.from('items').select('*')
 
   if (error) {
-    console.error("Error fetching data:", error);
-    return [];
+    console.error('Error fetching data:', error)
+    return []
   }
 
-  return data || [];
+  return data || []
 }
 
 export async function fetchItemById(id: number) {
   const { data, error } = await supabase
-    .from("items")
-    .select("*")
-    .eq("id", id)
-    .single();
+    .from('items')
+    .select('*')
+    .eq('id', id)
+    .single()
 
   if (error) {
-    console.error("Error fetching data:", error);
-    return null; // 오류가 발생할 경우 null 반환
+    console.error('Error fetching data:', error)
+    return null // 오류가 발생할 경우 null 반환
   }
 
-  return data;
+  return data
 }
 
-type BadgeType = "hot" | "fast";
+type BadgeType = 'hot' | 'fast'
 
 interface ItemType {
-  badge: BadgeType[];
-  category: string;
-  classification: string;
-  color: string | null;
-  deliveryperiod: number;
-  discount: number;
-  id: number;
-  imgurl: string;
-  origin: string;
-  originalprice: number;
-  size: string;
-  title: string;
+  badge: BadgeType[]
+  category: string
+  classification: string
+  color: string | null
+  deliveryperiod: number
+  discount: number
+  id: number
+  imgurl: string
+  origin: string
+  originalprice: number
+  size: string
+  title: string
 }
 
 interface FetchItemsResponse {
-  items: ItemType[];
-  nextCursor: number | null;
+  items: ItemType[]
+  nextCursor: number | null
 }
 
 export async function fetchItemsPerPage(
@@ -54,25 +54,25 @@ export async function fetchItemsPerPage(
   pageSize: number = 10,
   tab: number
 ): Promise<FetchItemsResponse> {
-  const start = pageParam * pageSize;
-  const end = start + pageSize - 1;
+  const start = pageParam * pageSize
+  const end = start + pageSize - 1
 
-  let query = supabase.from("items").select("*").range(start, end);
+  let query = supabase.from('items').select('*').range(start, end)
 
   if (tab != 0) {
-    const category = tabNumberToCategory(tab);
-    query = query.eq("category", `${category}`);
+    const category = tabNumberToCategory(tab)
+    query = query.eq('category', `${category}`)
   }
 
-  const { data, error } = await query;
+  const { data, error } = await query
 
   if (error) {
-    console.error("Error fetching data:", error);
-    return { items: [], nextCursor: null };
+    console.error('Error fetching data:', error)
+    return { items: [], nextCursor: null }
   }
 
-  const hasMore = data.length === pageSize;
-  const nextCursor = hasMore ? pageParam + 1 : null;
+  const hasMore = data.length === pageSize
+  const nextCursor = hasMore ? pageParam + 1 : null
 
-  return { items: data, nextCursor };
+  return { items: data, nextCursor }
 }
