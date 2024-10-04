4b87c5b9ae4205e43c4ee4fb4c9ff9d6754a0d42
chldls153@naver.com <chldls153@naver.com>
2024-10-04 17:22:20 +0900

    fix: PostCard 컴포넌트 유저이름 안보이는 오류 수정

--- src/components/Community/PostCard.tsx
+++ src/components/Community/PostCard.tsx
@@ -122,14 +122,11 @@ export default function PostCard({ post, onImageLoad }: PostCardProps) {
                           : 'http://img1.kakaocdn.net/thumb/R640x640.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg'
                       }
                     />
+                    {/* 프로필 클릭 시 모달 열림 */}
                     <Username onClick={handleUserInfoClick}>
-                      {' '}
-                      {/* 프로필 클릭 시 모달 열림 */}
-                      {userInfo?.nickname === ''
-                        ? userInfo?.email
-                          ? userInfo.email.split('@')[0]
-                          : null
-                        : userInfo?.nickname}
+                      {userInfo?.nickname
+                        ? userInfo?.nickname
+                        : userInfo.email.split('@')[0]}
                     </Username>
                   </Profile>
                 )}
